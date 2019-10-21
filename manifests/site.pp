892fde8232606f4f6805301cc05b141a313e425c
Pourya Mehdinejad <42834362+pmehdinejad@users.noreply.github.com>
2019-10-21 17:39:15 +0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,6 +9,6 @@ node 'puppet-server.cloudacropolis.com' {
   include role::master_server
   file { '/root/INFO':
   ensure => file,
-  content => $fqdn,
+  content => "Hostname: $[fqdn] and OS: $[os]\n",
   }
 }
