ed88b0d65fa22b1813e53f550175e316b138cbfe
Pourya Mehdinejad <42834362+pouryameh@users.noreply.github.com>
2019-10-29 14:35:28 +0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,16 +1,8 @@
 node default {
- file {'/root/README':
-    ensure  => file,
-    content => 'This is a readme',
-    owner   => 'root',
-    }
 }
 node 'puppet-server.cloudacropolis.com' {
   include role::master_server
-  file { '/root/INFO':
-  ensure => file,
-  content => "Hostname: ${fqdn} and OS: ${os.family}\n",
   }
 node 'puppet-agent.cloudacropolis.com' {
-  include profile::base-ubuntu.pp
+  include profile::base-ubuntu
 }
