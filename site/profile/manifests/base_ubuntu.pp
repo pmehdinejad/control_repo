022a7e032d788b3f1f5533532f21688de086106f
Pourya Mehdinejad <42834362+pouryameh@users.noreply.github.com>
2019-10-30 17:26:04 +0400

    Update base_ubuntu.pp

--- site/profile/manifests/base_ubuntu.pp
+++ site/profile/manifests/base_ubuntu.pp
@@ -1,10 +1,10 @@
 class profile::base_ubuntu {
   include profile::clamav
   include os_patching
-  include zabbix::agent
   class { 'zabbix::agent':
   zabbix_version => '3.0',
   server => '172.27.3.27',
+  zabbix_package_state => 'installed
 }
 
 }
